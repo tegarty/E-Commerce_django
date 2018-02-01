68a9b665af1020c943de645e8ede43759a6e437a
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-02-01 04:40:13 +0200

    delete review by the owner only

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 
 from .views import (
     AddReviewView,
+    DeleteReviewView,
     )
 
 urlpatterns = [
     url(r'^add_review/(?P<id>\d+)/$', AddReviewView.as_view(), name='add'),
+    url(r'^delete_review/(?P<id>\d+)/$', DeleteReviewView.as_view(), name='delete'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import DetailView, View, UpdateView, ListView, CreateView
+from django.views.generic import DetailView, View, UpdateView, ListView, CreateView, DeleteView
 from django.core.urlresolvers import reverse
 from django.urls import reverse_lazy
 from django.contrib import messages
@@ -33,3 +33,21 @@ class AddReviewView(View):
             )
             messages.success(request, 'Successfully added your product review!')
             return redirect('products:detail', slug=product.slug)
+
+
+class DeleteReviewView(View):
+    template_name = 'products/product_detail.html'
+
+    def post(self, request, id):
+        qs = Review.objects.filter(id=id)
+        if qs.exists() and qs.count() == 1:
+            review = qs.first()
+            current_user = self.request.user
+            username = review.user
+            if current_user == username:
+                review.delete()
+                messages.success(request, 'deleted successfully!')
+                return redirect('products:detail', slug=review.product.slug)
+            else:
+                messages.success(request, 'you can not delete this review because you don have the permission!')
+                return redirect('products:detail', slug=review.product.slug)
