a47dc95a28b61a1f7099a45d3e0409960d1095bf
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-02-01 08:41:52 +0200

    make forms in product detail html page as a component

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,3 +1,13 @@
 from django import forms
 
 from .models import Review
+
+
+# class UpdateReviewForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Review
+#         fields = [
+#             'review',
+#             'rate',
+#         ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import DetailView, View, UpdateView, ListView, CreateView, DeleteView
+from django.views.generic import DetailView, View, UpdateView, ListView, CreateView, DeleteView, FormView
 from django.core.urlresolvers import reverse
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.http import Http404, HttpResponseRedirect
 
 from .models import Review
-# from .forms import
+# from .forms import UpdateReviewForm
 from products.models import Product
 
 
